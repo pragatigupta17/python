2bf72ec2b37b2155fc52be5fa8a1119e59b2c077
Pragati Gupta <pragatigupta0104@gmail.com>
2024-10-02 15:20:10 +0530

    frist_commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Hello Python")
+print("Welcome","to","the world of python")#, comma generate a one space
+print("welcome"+"to"+"the world of python")# + plus did'nt give space between worlds
+print("welcom")
+print("to")
+print("python")
+# single comment start with #
+'''multiline
+comment
+start with '''
+# standard output function
+# print()//function=block of code ,()//parameter
+# n number of parameter.
+# by default line change
---
+++ second.py
@@ -0,0 +1,50 @@
+#end parameter
+print("welcome" , end=" ")
+print("to",end=" ")
+print("python classes")
+#-----------------------
+#sep parameter
+print("welcom","to","python",sep="$")
+
+#-----------------------------------------
+#indent
+#to define block in python we use indent,
+#it is equivalent to {} in c++,
+#    python ----> indent
+
+
+print("left most cornor uuyhdcdshhskcsdhfshfhdscdschdshfi\
+       chgfhfgjkhkhjlkhhiuhhkhh\
+       bjhhhukkjhkjhkjhjbkjhkjhih\
+       ghuhikhhhihiu")
+print("aaaaaaaa"
+      "hcdhvckzdcvlskdjiuds"
+      "jdnckdschids")
+
+#--------------------------
+#if body takes automatic space in next line
+
+if True:
+    print("if body")
+
+
+#tokens:---------------------------------------------
+#smallest unit of code which has a meaning to translate
+#there are five type of token
+'''1. keywords --- reserve words there 35 keywords true,false,etc
+keywords are the reserverd words which has a special meaning to compiler predefine words)
+'''
+import keyword
+print(keyword.kwlist)
+print("count",len(keyword.kwlist))
+'''2. identifier --- -- to identify the component of program uniquly we use identifire. 
+ 3. literal
+ 4. secial symbols(delimiter)
+ 5. operators'''
+print("PRAGATI")    # heres are three 3 tokens in this print function
+# print() identifire 
+#"welcome" literals(string)
+# #comment
+
+
+
