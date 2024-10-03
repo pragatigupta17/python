efcf0ea243c2e67c9eee77487c601926eb5e59be
Pragati Gupta <pragatigupta0104@gmail.com>
2024-10-03 15:31:30 +0530

    operator

---
+++ operator.py
@@ -0,0 +1,29 @@
+print(23%4) #==3
+print(4%23) #==4
+
+#// floor division operator (//)
+
+
+print(23/4) #==5.75
+print(23//4) #==5
+print(60//7) #==8
+print(60//6) #==10
+
+#expotinal operator
+print(4**5) #==1024
+print(7**3) #==343
+
+#comparsion operator
+print(23>45) #f
+print(23>=45) #f
+print(23<45) #t
+print(23<=45) #t
+print(23==45) #f
+print(23!=45) #t
+
+#string comparsion
+#ascii-->american standard code for information interchange
+print("apple" > "banana")   #ascii value A =65, a=97,0=48;/// its  done by lexicographical fromate
+print("pen" > "hjih")
+print("OranGe"<"orange")
+print("ABCD"<"ABC")
