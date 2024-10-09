f2eb5e8fb75a889d5559f1ed24acbbd54bb5df6c
Pragati Gupta <pragatigupta0104@gmail.com>
2024-10-09 15:38:18 +0530

    list

---
+++ index.py
@@ -0,0 +1,30 @@
+nums = [2,43,45,56,7,8,9,-33,44,45,5,6,45,66,45,]
+# index , index(element,startvalue,endvalue)
+a=nums.index(45)
+print(a)
+b=nums.index(45,a+1)
+print(b)
+c=nums.index(45,b+1)
+print(c)
+d=nums.index(45,c+1)
+print(d)
+
+'''INDEX---> index() return index value of element passed, 
+           we can give maximum of 3 parameter in index()
+           in first parameter , u have to pass the element itself,
+           second and third are optional parameter, if element is not found in list it throw error(exception).'''
+#           second --> start
+#           third  -->end(excluded)
+
+# COUNT---> it return the frequency of list no. means how many times a no. is present in list
+a=nums.count(2)
+print(a)
+a=nums.count(45)
+print(a)
+
+#SORT() -->it convert list in desscending to asscending order
+# None return, inplace fuction , no need of extra list(object)  
+nums.sort()
+print(nums)
+nums.sort(reverse=True) #asscending to desscending
+print(nums)
--- len_max.py
+++ len_max.py
@@ -5,3 +5,30 @@ m = max(lis1)
 print(m) #77
 m= max(lis)
 print(m) #77
+'''m= len(lis1)
+m=len(lis)'''
+
+nums =[2,3,4,5,6,78,9,-33,44]
+fruits =["apple","banana","grapes","pineapple"]
+
+#len()
+'''NOTE:- dot operator(methods)
+          function without using any dot operator'''
+
+a=len(nums)
+print(a)
+#max
+a=max(fruits)
+print(a)
+a=max(nums)
+print(a)
+#min
+a=min(fruits)
+print(a)
+a=min(nums)
+print(a)
+#sum
+a=sum(nums)
+print(a)
+a=sum(fruits)
+print(a)
