c299e06679727d01e225127601355859569e7571
Pragati Gupta <pragatigupta0104@gmail.com>
2024-10-25 21:15:37 +0530

    if_else

---
+++ if_else._cls2.py
@@ -0,0 +1,39 @@
+#data-type
+#########>
+#1)if-else
+#2)if-else ---> ladder
+#3)if-else ---> nesting
+print("main script")#simple
+if False:#complex
+    print("if body 1")
+    print("if body 2")
+    print("if body 3")
+    print("if body 4")
+    print("hii pragati")
+else :
+        print("else 1")
+        print("else 2")
+        print("else 3")
+        print("main script")
+        if True:
+            print("if body 1")
+            print("if body 2")
+            print("if body 3")
+            print("if body 4")
+            print("hiii pragati")
+else :
+       print("else 1") 
+       print("else 2") 
+       print("else 3") 
+# WAP to determain weather a char is wall or not
+"""ch = input("Enter any char : ")
+if(ch=='a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u'):
+print("vowel")
+else:
+    print("not vowel")"""
+    ch = input("enter any char")
+    v="AEIOUaeiou"
+    if(ch[0].in.v):
+        print("vowel")
+        else:
+            print("not vowel")
