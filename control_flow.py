a3e8adcb0facc3327419b503ea5c70c5ef2bafb4
Pragati Gupta <pragatigupta0104@gmail.com>
2024-11-08 15:16:28 +0530

    control_flow

---
+++ control_flow.py
@@ -0,0 +1,45 @@
+''' CONTROL FLOW 
+     1) sequantial control flow - it excute every line of code
+     2) conditional control flow - it takes limitation like which lines we want to execute we can excute , 
+     there are types of condition statement ---
+           1)if ,
+           2)if-else,
+           3)if-elseif,
+           4)if-elseif-else.
+
+     3) iterative/looping control flow - it execute at least at one time, some type of looping
+           1)While loop -(infinite-time execution) 
+           2)for loop - (final-time execution)
+     4) transfer statement - 1)continue,
+                             2)break,
+                             3)pass'''
+# --------------------------------------------------------------------------------------#
+'''flow chart - if else
+                statement 1
+                    |
+                    |
+                statement2
+                     |
+                     |
+                if (condition)------T
+                      |             |
+                      | F        statement3
+                statement4          |    
+                      |             |
+                      |             |    
+                      ---------------  
+                      |
+                      |
+                statement5 (final statement)  '''
+# SYNTAX OF IF-ELSE
+x=10
+y=20
+if x==y:
+    print("both are equql")
+else:
+    print("hello")
+                    
+
+
+                            
+                            
---
+++ range.py
@@ -0,0 +1,42 @@
+# # RANGE:- 
+# # syntax :- range(start,stop,step(direction)) [1:11:1]
+# """ x = range(1,11,1)
+#         print(list(x))"""
+# """ 1) range(stop)
+#     2) range(start,stop) 
+#     3) range(start,stop,step/direction)"""
+# x1 =(2,4,6,8,10)
+# x = (1,3,5,7,9)
+# x1 = range(2,11,2)
+# x = range(1,10,2)
+# print(x1)
+# print (x)
+# print(list(x))
+# u=range(10)
+# print(u)
+# i=range(3,9)
+# print(i)
+# c=range(5,10,-1)
+# print(list(c))
+# s=[-3,-2,-1,0,1,2,3]
+# s1=range(-3,4,1)
+# print(s)
+# d=[2]
+# x=range(2,3)
+# print(d)
+# t=[-2]
+# t1=range(-2,4)
+# print(t)
+# # y=[]
+# y1=range([1,5,-1])
+# print(y1)
+# r=[2,4,6,8,1,3,5,7,9]
+# x=range(2,1,9)
+# print(list(r))
+# my_str='pragati'
+# print(my_str [0:7] [0:4] [0:2])
+
+
+
+y1=range(1,5,-1)
+print(list(y1))
