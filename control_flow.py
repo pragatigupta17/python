c23d3c659ba9e2ae205c24ba33ce95149da37735
Pragati Gupta <pragatigupta0104@gmail.com>
2024-11-09 15:23:16 +0530

    while_loop

--- control_flow.py
+++ control_flow.py
@@ -39,6 +39,22 @@ if x==y:
 else:
     print("hello")
                     
+# SYNTAX OF if-elif-else
+a=int (input("enter 1st value"))
+print("1.add\n 2.sub\n 3.div\n 4.multiply")
+n=int(input("enter your choice no."))
+b=int (input("enter 2nd value"))
+if n==1:
+    print(a+b)
+elif n==2:
+    print(a-b)
+elif n==3:
+    print(a/b)
+elif n==4:
+    print(a*b)
+else:
+    print("plese enter correct no")
+
 
 
                             
---
+++ while_loop.py
@@ -0,0 +1,20 @@
+# WHILE_LOOP ---> 
+''' 1) when loop is (infinite iteration) no increment or decrement. 
+    2) when loop is (finite iteration) increment or decrement is done.'''
+# FOR_LOOP --->
+'''(finite-iteration)'''
+n=int(input("enter any no:"))
+i=1
+while i<=100:
+    if i<100:
+      print(i,end=' ')
+    else:
+        print(i)
+    i=i+1
+#--------------------------------------------
+'''HOME WORK ---> 
+    2,4,6,8,10 even no and sum it 
+    2,4,6,8,10,12,14,16,18,20 sum 
+    1,3,5,7,9 odd no sum
+    1,3,5,7,9,11,13,15,17,19 sum
+    1+2+3+4+5+6+7+8=9+10 = total sum '''
