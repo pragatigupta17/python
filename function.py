e92ee58db820c0679c0f0cba0d3c0546485c4f11
Pragati Gupta <pragatigupta0104@gmail.com>
2024-11-20 15:48:28 +0530

    function

--- function.py
+++ function.py
@@ -1,5 +1,5 @@
 #POSITIONAL ARGUMENT OF FUNCTION
-def add (x,y,z):
+'''def add (x,y,z):
     z=x+y+z
     print(z)
     #return z
@@ -8,3 +8,23 @@ q=int(input("enter 2nd value"))
 r=int(input("enter 3rd value"))
 x=add(p,q,r)# (y=r,x=p,z=q) this KEYWORD ARGUMENT 
 print(x)
+'''
+#KEYWORD ARGUMENT OF FUNCTION
+# def add(*n):
+#     print(n)
+# 	sum=0
+# 	for i in n:
+# 	    sum = sum+i
+# 	return sum
+# var=add(10,20,30,40,50,60)
+# print(var)
+
+
+def add(*n):
+    print(n)
+    sum=0
+    for i in n:
+        sum = sum+i
+    return sum
+var=add(10,20,30,40,50,60)
+print(var)
